ac10c8ca1a5f593e6459b264d1cfb55ee6392ada
Tarakur Rahman <39719842+tarakurrahman@users.noreply.github.com>
2018-07-12 11:34:21 -0400

    Able to deal cards to the players
    - Set up _Main Camera in the inspector
    - Set up the game layout
    - Created the player class
    - Deleted the LayoutXML, Layout (Script) and Prospector(Script) class because they were not allowing the game to run
    - Fanning the hand
    Next step: A Tiny Introduction to LINQ

--- Assets/__Scripts/Bartok.cs
+++ Assets/__Scripts/Bartok.cs
@@ -10,11 +10,17 @@ public class Bartok : MonoBehaviour {
     public TextAsset deckXML;
     public TextAsset layoutXML;
     public Vector3 layoutCenter = Vector3.zero;
+    public float handFanDegrees = 10f; // a
 
     [Header("Set Dynamically")]
     public Deck deck;
     public List<CardBartok> drawPile;
     public List<CardBartok> discardPile;
+    public List<Player> players; // b
+    public CardBartok targetCard;
+
+    private BartokLayout layout;
+    private Transform layoutAnchor;
 
     void Awake() {
         S = this;
@@ -24,5 +30,82 @@ public class Bartok : MonoBehaviour {
         deck = GetComponent<Deck>(); // Get the Deck
         deck.InitDeck(deckXML.text); // Pass DeckXML to it
         Deck.Shuffle(ref deck.cards); // This shuffles the deck // a
+
+        layout = GetComponent<BartokLayout>(); // Get the Layout
+        layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
+
+        drawPile = UpgradeCardsList(deck.cards);
+        LayoutGame();
+    }
+
+    List<CardBartok> UpgradeCardsList(List<Card> lCD) { // a
+        List<CardBartok> lCB = new List<CardBartok>();
+        foreach (Card tCD in lCD) {
+            lCB.Add(tCD as CardBartok);
+        }
+        return (lCB);
+    }
+
+    // Position all the cards in the drawPile properly
+    public void ArrangeDrawPile() {
+        CardBartok tCB;
+
+        for (int i = 0; i < drawPile.Count; i++) {
+            tCB = drawPile[i];
+            tCB.transform.SetParent(layoutAnchor);
+            tCB.transform.localPosition = layout.drawPile.pos;
+            // Rotation should start at 0
+            tCB.faceUp = false;
+            tCB.SetSortingLayerName(layout.drawPile.layerName);
+            tCB.SetSortOrder(-i * 4); // Order them front-to-back
+            tCB.state = CBState.drawpile;
+        }
+    }
+
+    // Perform the initial game layout
+    void LayoutGame() {
+        // Create an empty GameObject to serve as the tableau's anchor // c
+        if (layoutAnchor == null) {
+            GameObject tGO = new GameObject("_LayoutAnchor");
+            layoutAnchor = tGO.transform;
+            layoutAnchor.transform.position = layoutCenter;
+        }
+
+        // Position the drawPile cards
+        ArrangeDrawPile();
+        
+        // Set up the players
+        Player pl;
+        players = new List<Player>();
+        foreach (SlotDef tSD in layout.slotDefs) {
+            pl = new Player();
+            pl.handSlotDef = tSD;
+            players.Add(pl);
+            pl.playerNum = tSD.player;
+        }
+        players[0].type = PlayerType.human; // Make only the 0th player human
+    }
+
+    // The Draw function will pull a single card from the drawPile and return it
+    public CardBartok Draw() {
+        CardBartok cd = drawPile[0]; // Pull the 0th CardProspector
+        drawPile.RemoveAt(0); // Then remove it from List<> drawPile
+        return (cd); // And return it
+    }
+
+    // This Update() is temporarily used to test adding cards to players' hands
+    void Update() { // d
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            players[0].AddCard(Draw());
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            players[1].AddCard(Draw());
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            players[2].AddCard(Draw());
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4)) {
+            players[3].AddCard(Draw());
+        }
     }
 }
---
+++ Assets/__Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq; // Enables LINQ queries, which will be explained soon
+// The player can either be human or an ai
+public enum PlayerType {
+    human,
+    ai
+}
+
+[System.Serializable] // a
+public class Player { // b
+    public PlayerType type = PlayerType.ai;
+    public int playerNum;
+    public SlotDef handSlotDef;
+    public List<CardBartok> hand; // The cards in this player's hand
+                    
+    // Add a card to the hand
+    public CardBartok AddCard(CardBartok eCB) {
+        if (hand == null) hand = new List<CardBartok>();
+  
+        // Add the card to the hand
+        hand.Add(eCB);
+        FanHand();
+        return (eCB);
+    }
+
+    // Remove a card from the hand
+    public CardBartok RemoveCard(CardBartok cb) {
+        // If hand is null or doesn't contain cb, return null
+        if (hand == null || !hand.Contains(cb)) return null;
+        hand.Remove(cb);
+        FanHand();
+        return (cb);
+    }
+
+    public void FanHand() { // a
+      // startRot is the rotation about Z of the first card // b
+        float startRot = 0;
+        startRot = handSlotDef.rot;
+        if (hand.Count > 1) {
+            startRot += Bartok.S.handFanDegrees * (hand.Count - 1) / 2;
+        }
+
+        // Move all the cards to their new positions
+        Vector3 pos;
+        float rot;
+        Quaternion rotQ;
+        for (int i = 0; i < hand.Count; i++) {
+            rot = startRot - Bartok.S.handFanDegrees * i;
+            rotQ = Quaternion.Euler(0, 0, rot); // c
+
+            pos = Vector3.up * CardBartok.CARD_HEIGHT / 2f; // d
+            pos = rotQ * pos; // e
+            
+            // Add the base position of the player's hand (which will be at the            
+            // bottom-center of the fan of the cards)
+            pos += handSlotDef.pos; // f
+            pos.z = -0.5f * i; // g
+
+            // Set the localPosition and rotation of the ith card in the hand
+            hand[i].transform.localPosition = pos; // h
+            hand[i].transform.rotation = rotQ;
+            hand[i].state = CBState.hand;
+
+            hand[i].faceUp = (type == PlayerType.human); // i
+            
+            // Set the SortOrder of the cards so that they overlap properly
+            hand[i].SetSortOrder(i * 4); // j
+        }
+    }
+}

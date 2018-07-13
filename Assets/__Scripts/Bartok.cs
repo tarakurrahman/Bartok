4ec9d8b8c2864675e67badf615157448ffd28ccb
TR347 <TR347@CILABS-114B-08.scilsnet.rutgers.edu>
2018-07-12 20:25:07 -0400

    The prototype is complete
    - Added the UI elements
    - Added the game over logic

--- Assets/__Scripts/Bartok.cs
+++ Assets/__Scripts/Bartok.cs
@@ -142,6 +142,10 @@ public class Bartok : MonoBehaviour {
         int lastPlayerNum = -1;
         if (CURRENT_PLAYER != null) {
             lastPlayerNum = CURRENT_PLAYER.playerNum;
+            // Check for Game Over and need to reshuffle discards
+            if (CheckGameOver()) {
+                return; // a
+            }
         }
         CURRENT_PLAYER = players[num];
         phase = TurnPhase.pre;
@@ -150,6 +154,35 @@ public class Bartok : MonoBehaviour {
         
         // Report the turn passing
         Utils.tr("Bartok:PassTurn()", "Old: " + lastPlayerNum, "New: " + CURRENT_PLAYER.playerNum); // h
+    }
+
+    public bool CheckGameOver() {
+        // See if we need to reshuffle the discard pile into the draw pile
+        if (drawPile.Count == 0) {
+            List<Card> cards = new List<Card>();
+            foreach (CardBartok cb in discardPile) {
+                cards.Add(cb);
+            }
+            discardPile.Clear();
+            Deck.Shuffle(ref cards);
+            drawPile = UpgradeCardsList(cards);
+            ArrangeDrawPile();
+        }
+
+        // Check to see if the current player has won
+        if (CURRENT_PLAYER.hand.Count == 0) {
+            // The player that just played has won!
+            phase = TurnPhase.gameOver;
+            Invoke("RestartGame", 1); // b
+            return (true);
+        }
+
+        return (false);
+    }
+
+    public void RestartGame() {
+        CURRENT_PLAYER = null;
+        SceneManager.LoadScene("__Bartok_Scene_0");
     }
 
     // ValidPlay verifies that the card chosen can be played on the discard pile
---
+++ Assets/__Scripts/GameOverUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required for the uGUI classes like Text
+
+public class GameOverUI : MonoBehaviour {
+    private Text txt;
+    void Awake() {
+        txt = GetComponent<Text>();
+        txt.text = "";
+    }
+
+    void Update() {
+        if (Bartok.S.phase != TurnPhase.gameOver) {
+            txt.text = "";
+            return;
+        }
+        // We only get here if the game is over
+        if (Bartok.CURRENT_PLAYER == null) return; // a
+        if (Bartok.CURRENT_PLAYER.type == PlayerType.human) {
+            txt.text = "You won!";
+        } else {
+            txt.text = "Game Over";
+        }
+    }
+}
